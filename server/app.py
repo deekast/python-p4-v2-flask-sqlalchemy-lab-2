a909809d4090cd8744672aea63a75a7dbc1fb87f
deekast <96024516+deekast@users.noreply.github.com>
2024-05-15 14:55:44 -0400

    first two objectives done

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_migrate import Migrate
 
-from models import db
+from models import db, Customer, Item, Review 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -15,6 +15,22 @@ db.init_app(app)
 @app.route('/')
 def index():
     return '<h1>Flask SQLAlchemy Lab 2</h1>'
+
+@app.route('/customer')
+def customer():
+    return '<h1>Customer</h1>'
+
+@app.route('/item')
+def item():
+    return '<h1>Item</h1>'
+
+@app.route('/review')
+def review():
+    return '<h1>Review</h1>'
+
+@app.get('/customer')
+def get_customer():
+    return Customer.query.all()
 
 
 if __name__ == '__main__':
